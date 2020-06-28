67161852603e27d9d3b7cfd60d7ad52c2e86616f
shreygarg98 <30731190+shreygarg98@users.noreply.github.com>
2020-06-28 19:53:59 +0530

    Update views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -66,7 +66,7 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
-            app.logger.info('%s failed to log in, invalid username or password', form.username.data)
+            app.logger.info('Wrong Credentials!')
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
